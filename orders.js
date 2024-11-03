2f0565f8141bb9037801bc7abdd1298f060198c2
Qasim Nawaz <qasimtariqsays@gmail.com>
2024-11-03 16:32:52 +0500

    Add files via upload

--- orders.js
+++ orders.js
@@ -1,50 +1,12 @@
-import Order from '../models/orders.js';
-import asyncHandler from 'express-async-handler';
+import express from "express";
+const orderRoutes = express.Router();
+import { requiredLoggedIn, checkAdmin} from '../middlewares/authMiddleware.js';
 
+import * as order from "../controllers/orders.js";
 
-const fetchUserOrders = asyncHandler(async (req, res) => {
-
-    const orders = await Order.find({ user: req.params.id });
-	if (orders) {
-		res.json(orders);
-	} else {
-		res.json({ error: 'No orders could be found.' });
-		throw new Error('No Orders found.');
-	}
-
-});
-
-const fetchOrders = asyncHandler(async (req, res) => {
-	const orders = await Order.find({});
-	res.json(orders);
-});
-
-const deleteOrder = asyncHandler(async (req, res) => {
-	const order = await Order.findByIdAndDelete(req.params.id);
-	if (order) {
-		res.json(order);
-	} else {
-		res.send('Order not found.');
-		throw new Error('Order not found.');
-	}
-});
-
-const setDelivered = asyncHandler(async (req, res) => {
-	const order = await Order.findById(req.params.id);
-
-	if (order) {
-		order.isDelivered = true;
-		const updatedOrder = await order.save();
-		res.json(updatedOrder);
-	} else {
-		res.send('Order could not be uploaded.');
-		throw new Error('Order could not be updated.');
-	}
-});
-
-export { 
-    fetchUserOrders,
-	fetchOrders,
-	deleteOrder,
-	setDelivered
-};
+orderRoutes.route('/:id').get(requiredLoggedIn, order.fetchUserOrders);
+orderRoutes.route('/').get(requiredLoggedIn, checkAdmin, order.fetchOrders);
+orderRoutes.route('/:id').put(requiredLoggedIn, checkAdmin, order.setDelivered);
+orderRoutes.route('/:id').delete(requiredLoggedIn, checkAdmin, order.deleteOrder);
+ 
+export default orderRoutes;
--- stripe.js
+++ stripe.js
@@ -1,67 +1,9 @@
-import dotenv from 'dotenv';
-dotenv.config();
+import express from 'express';
+const stripeRoute = express.Router();
+import { requiredLoggedIn } from '../middlewares/authMiddleware.js';
 
-import Stripe from 'stripe';
-import Order from '../models/orders.js';
-import Product from '../models/products.js';
+import * as stripe from "../controllers/stripe.js";
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+stripeRoute.route('/').post(requiredLoggedIn, stripe.stripePayment);
 
-  export const stripePayment = async (req, res) => {
-    const data = req.body;
-    console.log(data);
-  
-    let lineItems = [];
-  
-    if (data.shipping == 300.99) {
-      lineItems.push({
-        price: process.env.EXPRESS_SHIPPING_ID,
-        quantity: 1,
-      });
-    } else {
-      lineItems.push({
-        price: process.env.STANDARD_SHIPPING_ID,
-        quantity: 1,
-      });
-    }
-  
-    data.cartItems.forEach((item) => {
-      lineItems.push({
-        price: item.stripeId,
-        quantity: item.qty,
-      });
-    });
-  
-    const session = await stripe.checkout.sessions.create({
-      line_items: lineItems,
-      mode: 'payment',
-      success_url: 'http://localhost:5173/success',
-      cancel_url: 'http://localhost:5173/cancel',
-    });
-  
-    const order = new Order({
-      orderItems: data.cartItems,
-      user: data.userInfo._id,
-      username: data.userInfo.name,
-      email: data.userInfo.email,
-      shippingAddress: data.shippingAddress,
-      shippingPrice: data.shipping,
-      subtotal: data.subtotal,
-      totalPrice: Number(data.subtotal + data.shipping).toFixed(2),
-    });
-  
-    const newOrder = await order.save();
-  
-    data.cartItems.forEach(async (cartItem) => {
-      let product = await Product.findById(cartItem.id);
-      product.stock = product.stock - cartItem.qty;
-      product.save();
-    });
-  
-    res.send(
-      JSON.stringify({
-        orderId: newOrder._id.toString(),
-        url: session.url,
-      })
-    );
-  };
+export default stripeRoute;
